d54efef1a9308f64a54bd6b14dd80c614aa57e77
smoke0030 <ft.priest@gmail.com>
2023-06-13 21:04:33 +0300

    изменен цвет для LaunchScreen. Добавлены лейбл и кнопка в main.storyboard. Написана логика для изменения количества нажатий в ViewController

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
+    private var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "Значение счетчика: \(count)"
     }
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+        count += 1
+        countLabel.text = "Значение счетчика: \(count)"
+        }
+    }
+    
 
-}
 

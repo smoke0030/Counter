644b76b12f8198fe0f4c0c956e6b41f2b8b93898
smoke0030 <ft.priest@gmail.com>
2023-06-14 20:24:53 +0300

    изменено много всего

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,20 +10,69 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var countLabel: UILabel!
-    @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var historyLabel: UITextView!
+    @IBOutlet weak var plusButtonOutlet: UIButton!
+    @IBOutlet weak var minusButtonOutlet: UIButton!
+    @IBOutlet weak var clearButtonOutlet: UIButton!
+    
     private var count: Int = 0
+    private var historyArray: [String] = []
+    let date = Date()
+    let dateFormatter = DateFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        historyLabel.isEditable = false
+        plusButtonOutlet.setImage(UIImage(systemName: "plus"), for: .normal)
+        minusButtonOutlet.setImage(UIImage(systemName: "minus"), for: .normal)
+        plusButtonOutlet.backgroundColor = .red
+        minusButtonOutlet.backgroundColor = .blue
+        clearButtonOutlet.backgroundColor = .green
+        clearButtonOutlet.setImage(UIImage(systemName: "clear"), for: .normal)
+        
         
     }
-
-    @IBAction func buttonAction(_ sender: UIButton) {
-        count += 1
+    private func updateInfo() {
+        historyLabel.text = historyArray.joined(separator: "\n")
         countLabel.text = "Значение счетчика: \(count)"
-        }
+        
     }
     
-
+    @IBAction func buttonPlus(_ sender: UIButton) {
+        count += 1
+        historyArray.append("\(date.toJustTime): значение изменено на +1")
+        updateInfo()
+        
+    }
+    
+    @IBAction func buttonMinus(_ sender: UIButton) {
+        count -= 1
+        if count < 0 {
+            count = 0
+            historyArray.append("\(date.toJustTime) попытка уменьшить значение счётчика ниже 0")
+        } else {
+            historyArray.append("\(date.toJustTime): значение изменено на -1")
+        }
+        updateInfo()
+    }
+    
+    @IBAction func buttonClear(_ sender: UIButton) {
+        count = 0
+        historyArray.append("\(date.toJustTime): значение сброшено")
+        updateInfo()
+        
+    }
+    
+    
+    }
+    
+extension Date {
+    var toJustTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss dd MMMM yyyy"
+        return  dateFormatter.string(from: self)
+    }
+}
 
